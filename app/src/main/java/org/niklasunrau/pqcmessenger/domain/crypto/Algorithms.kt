c86d0df1c4b841fd207857b20d792c1059344db3
Niklas267 <71382218+Niklas267@users.noreply.github.com>
2024-02-27 15:25:15 +0100

    Fixed accidental test algorithm

--- app/src/main/java/org/niklasunrau/pqcmessenger/domain/crypto/Algorithms.kt
+++ app/src/main/java/org/niklasunrau/pqcmessenger/domain/crypto/Algorithms.kt
@@ -1,15 +1,12 @@
 package org.niklasunrau.pqcmessenger.domain.crypto
 
 import org.niklasunrau.pqcmessenger.domain.crypto.mceliece.McEliece
-import org.niklasunrau.pqcmessenger.domain.crypto.tmp.TestAlg
 
 object Algorithms {
     enum class Type{
-        MCELIECE,
-        TEST
+        MCELIECE
     }
     val map: Map<Type, AsymmetricAlgorithm<AsymmetricSecretKey, AsymmetricPublicKey>> = mapOf(
-        Type.MCELIECE to McEliece,
-        Type.TEST to TestAlg
+        Type.MCELIECE to McEliece
     )
 }
